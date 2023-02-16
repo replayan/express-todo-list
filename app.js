3316e6d68e2b11c6206feed3f2c962de7bacecbc
Ayan Chakraborty <ac4462383@gmail.com>
2023-02-17 01:47:41 +0530

    files added

---
+++ app.js
@@ -0,0 +1,38 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const date = require(__dirname + "/date.js");
+// console.log(date());
+const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  let day = date.getDate();
+  res.render("list", { listTitle: day, newListItems: items });
+
+  // res.render("list", { listTitle: date, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
