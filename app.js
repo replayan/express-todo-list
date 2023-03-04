fb4a5e3e77444e05ef88a75514efa057f75f40fd
Ayan Chakraborty <ac4462383@gmail.com>
2023-03-05 01:10:37 +0530

    items added to db

--- app.js
+++ app.js
@@ -3,25 +3,65 @@ const express = require("express");
 const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 const app = express();
-
-// let items = ["Buy Food", "Cook Food", "Eat Food"];
-// let workItems = [];
-
-mongoose.set("strictQuery", true);
-mongoose.connect("mongodb://127.0.0.1:27017/testDB");
-
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://127.0.0.1:27017/testDB");
+
+const itemSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!",
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item.",
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item.",
+});
+
+const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems)
+//   .then(function () {
+//     console.log("Successfully saved defult items to DB");
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
 
 app.get("/", (req, res) => {
   let day = date.getDate();
-  res.render("list", { listTitle: day, newListItems: items });
+  // res.render("list", { listTitle: day, newListItems: items });
+
+  Item.find({})
+    .then((foundItem) => {
+      if (foundItem.length === 0) {
+        return Item.insertMany(defaultItems);
+      } else {
+        return foundItem;
+      }
+    })
+    .then(function (foundItems) {
+      res.render("list", { listTitle: day, newListItems: foundItems });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 });
 
 app.post("/", (req, res) => {
   const item = req.body.newItem;
+  // const item to be added to the DB
 
+  // to be added to the DB
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
